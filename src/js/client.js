1d1e513cc9cc17f652783d39f17ec919e9e8c7c3
Takahiro Ito <takahiro0606@gmail.com>
2019-12-10 15:49:52 -0800

    initial push

---
+++ src/js/client.js
@@ -0,0 +1,45 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Layout extends React.Component {
+    // constructor to 
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            viewText: "home",
+            viewFlg: 1
+        };
+    }
+
+    // switch the state
+    renderChange() {
+        let newFlg = this.state.viewFlg == 1 ? 0 : 1;
+        this.setState({ viewFlg: newFlg });
+    }
+
+    // you can render DOM object
+    renderTextDom() {
+        return (
+            <p className='text-center'>
+                This is {this.state.viewFlg == 1 ? this.state.viewText : "sub page"} page.
+            </p>
+        );
+    }
+
+    // returning text
+    renderText() {
+        return "Hey hey! we are MeshiMuda!";
+    }
+
+    render() {
+        return (
+            <div>
+                <h1 className='text-center'>{this.renderText()}</h1>
+                {this.renderTextDom()}
+                <button className='text-center' onClick={() => this.renderChange()}>Change</button>
+            </div>
+        );
+    }
+}
+const app = document.getElementById("app");
+ReactDOM.render(<Layout />, app);
